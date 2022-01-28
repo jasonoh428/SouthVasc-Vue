fa41aea89d0a45db2c1d96d28db4936416123183
jasonoh428 <ohjason794@gmail.com>
2022-01-28 14:26:39 -0600

    updated nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,17 +20,17 @@ export default {
         src: "https://www.googletagmanager.com/gtag/js?id=UA-215187232-1",
         async: true,
       },
-      {
-        vmid: "google-tag",
-        type: "text/javascript",
-        innerHTML: `
-        window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-215187232-1');
-        `,
-      },
+      //   {
+      //     vmid: "google-tag",
+      //     type: "text/javascript",
+      //     innerHTML: `
+      //     window.dataLayer = window.dataLayer || [];
+      // function gtag(){dataLayer.push(arguments);}
+      // gtag('js', new Date());
+      //
+      // gtag('config', 'UA-215187232-1');
+      //     `,
+      //   },
       {
         vmid: "twitter",
 
