497467178c84ce2fbcb75130302a5fbc9b011aea
jasonoh428 <ohjason794@gmail.com>
2022-01-28 14:12:08 -0600

    added google tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,19 @@ export default {
 
     script: [
       {
+        src: "https://www.googletagmanager.com/gtag/js?id=UA-215187232-1",
+        async: true,
+      },
+      {
+        innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-215187232-1');
+        `,
+      },
+      {
         vmid: "twitter",
 
         defer: true,
